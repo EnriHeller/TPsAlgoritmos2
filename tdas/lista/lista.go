6047b77054dcfaed46c7f48bfee53e020d9c1a4b
EnriHeller <enrique.heller12@gmail.com>
2024-04-24 14:34:13 -0300

    test functions, first version

--- tdas/lista/lista.go
+++ tdas/lista/lista.go
@@ -2,14 +2,30 @@ package lista
 
 type Lista[T any] interface {
 
-	//Indica a partir de un booleano, si la lista se encuentra vacia.
+	//Si la lista se encuentra vacia, devuelve verdadero. En caso contrario, devuelve falso
 	EstaVacia() bool
+
+	//Inserta un nuevo elemento al principio de la lista.
 	InsertarPrimero(T)
+
+	//Inserta un nuevo elemento al final de la lista.
 	InsertarUltimo(T)
+
+	//Borra el primer elemento de la lista. En caso de que este vacia, entra en pánico con un mensaje "La lista está vacia"
 	BorrarPrimero() T
+
+	//Obtiene el valor del primer elemento de la lista. En caso de que este vacia, entra en pánico con un mensaje "La lista está vacia".
 	VerPrimero() T
+
+	//Obtiene el valor del último elemento de la lista. En caso de que este vacia, entra en pánico con un mensaje "La lista está vacia".
 	VerUltimo() T
+
+	//Devuelve la cantidad de elementos alojados en la lista.
 	Largo() int
+
+	//Recorre la lista, aplicandole a cada elemento la función pasada por parámetro hasta encontrar una condición de corte. Si la función devuelve false, significa que se encontro una condición de corte. 
 	Iterar(visitar func(T) bool)
+
+	//Permite acceder a un iterador externo, asociado a la lista actual.
 	Iterador() IteradorLista[T]
 }
---
+++ tdas/lista/lista_test.go
@@ -0,0 +1,38 @@
+package lista_test
+
+import(
+	"fmt"
+	"testing"
+	TDALista "tdas/lista"
+)
+
+
+//Al insertar un elemento en la posición en la que se crea el iterador, efectivamente se inserta al principio.
+func InsertarAlInicio(t *testing.T){
+
+}
+
+//Insertar un elemento cuando el iterador está al final efectivamente es equivalente a insertar al final.
+func InsertarAlFinal(t *testing.T){
+
+}
+
+//Insertar un elemento en el medio se hace en la posición correcta.
+func InsertarEnMedio(t *testing.T){
+
+}
+
+//Al remover el elemento cuando se crea el iterador, cambia el primer elemento de la lista.
+func CambiaPrimerElementoAlRemover(t *testing.T){
+
+}
+
+//Remover el último elemento con el iterador cambia el último de la lista.
+func CambiaUltimoElementoAlRemover(t *testing.T){
+
+}
+
+//Verificar que al remover un elemento del medio, este no está.
+func NoExisteMedioAlRemoverlo(t *testing.T){
+
+}
