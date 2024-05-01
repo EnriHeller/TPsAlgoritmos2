289a5a28c9f916b61c996c10711cc2c99576c7b7
EnriHeller <enrique.heller12@gmail.com>
2024-05-01 20:02:55 -0300

    avances

--- tdas/diccionario/hash.go
+++ tdas/diccionario/hash.go
@@ -1,12 +1,16 @@
 package diccionario
 
+import (
+	"fmt"
+)
+
 const (
-	VACIO int = 0
+	VACIO   int = 0
 	OCUPADO int = 1
 	BORRADO int = 2
 
 	TAMANO int = 6
-) 
+)
 
 type celdaHash[K comparable, V any] struct {
 	clave  K
@@ -15,33 +19,33 @@ type celdaHash[K comparable, V any] struct {
 }
 
 type hashCerrado[K comparable, V any] struct {
-	tabla    []celdaHash[K,V]
+	tabla    []celdaHash[K, V]
 	cantidad int
 	tam      int
 	borrados int
 }
 
-type iterDiccionario[K comparable, V any] struct{
+type iterDiccionario[K comparable, V any] struct {
 	hash hashCerrado[K, V]
-	pos int
+	pos  int
 }
 
-func crearCeldaHash[K comparable, V any]() celdaHash[K, V]{
+func crearCeldaHash[K comparable, V any]() celdaHash[K, V] {
 	nuevaCelda := new(celdaHash[K, V])
 	nuevaCelda.estado = 0
 	return *nuevaCelda
 }
 
-func CrearHash[K comparable, V any]() Diccionario[K, V]{
-	
+func CrearHash[K comparable, V any]() Diccionario[K, V] {
+
 	nuevoHash := new(hashCerrado[K, V])
 	nuevoHash.tam = TAMANO
 	nuevoHash.cantidad = 0
 	nuevoHash.borrados = 0
 
-	nuevaTabla := make([]celdaHash[K,V], nuevoHash.tam) 
+	nuevaTabla := make([]celdaHash[K, V], nuevoHash.tam)
 
-	for i := 0 ; i < nuevoHash.tam ; i ++{
+	for i := 0; i < nuevoHash.tam; i++ {
 		nuevaCelda := crearCeldaHash[K, V]()
 		nuevaTabla = append(nuevaTabla, nuevaCelda)
 	}
@@ -51,49 +55,97 @@ func CrearHash[K comparable, V any]() Diccionario[K, V]{
 	return nuevoHash
 }
 
-func (hash *hashCerrado[K, V]) Guardar(clave K, dato V){
+func (hash *hashCerrado[K, V]) Guardar(clave K, dato V) {
 
 }
 
-func (hash *hashCerrado[K, V]) Pertenece(clave K) bool{
+func (hash *hashCerrado[K, V]) Pertenece(clave K) bool {
 	return true
 }
 
-func (hash *hashCerrado[K, V]) Obtener(clave K) V{
+func (hash *hashCerrado[K, V]) Obtener(clave K) V {
+	pos := hash.buscar(clave)
+	return hash.tabla[pos].dato
+}
+
+func (hash *hashCerrado[K, V]) Borrar(clave K) V {
 	var dato V
 	return dato
 }
 
-func (hash *hashCerrado[K, V]) Borrar(clave K) V{
-	var dato V
-	return dato
-}
-
-func (hash *hashCerrado[K, V]) Cantidad() int{
+func (hash *hashCerrado[K, V]) Cantidad() int {
 	return hash.cantidad
 }
 
-func (hash *hashCerrado[K, V]) Iterar(visitar func(clave K, valor V) bool){
+func (hash *hashCerrado[K, V]) Iterar(visitar func(clave K, valor V) bool) {
 
 }
 
-func (hash *hashCerrado[K, V]) Iterador() IterDiccionario[K, V]{
+func convertirABytes[K comparable](clave K) []byte {
 
-	return &iterDiccionario[K,V]{}
+	return []byte(fmt.Sprintf("%v", clave))
 }
 
-func (iter *iterDiccionario[K, V]) HaySiguiente() bool{
+func sdbmHash(data []byte) uint64 {
+	var hash uint64
+
+	for _, b := range data {
+		hash = uint64(b) + (hash << 6) + (hash << 16) - hash
+	}
+
+	return hash
+}
+
+func (hash *hashCerrado[K, V]) buscar(clave K) int {
+	posicion := hash.hashear(clave)
+	primeraPorcion := hash.tabla[posicion: hash.tam]
+	porcionAuxiliar := hash.tabla[:posicion]
+
+	for i:= posicion; i < hash.tam; i++{
+		celdaActual := primeraPorcion[i]
+		if celdaActual.estado == OCUPADO && celdaActual.clave == clave{
+			return posicion
+		}else if celdaActual.estado == VACIO{
+			panic("La clave no pertenece al diccionario")
+		}else{
+			continue
+		}
+	}
+
+	for _ , celdaActual := range(porcionAuxiliar){
+		if celdaActual.estado == OCUPADO && celdaActual.clave == clave{
+			return posicion
+		}else if celdaActual.estado == VACIO{
+			panic("La clave no pertenece al diccionario")
+		}else{
+			continue
+		}
+	}
+
+	return posicion
+}
+
+func (hash *hashCerrado[K, V]) hashear(clave K) int {
+	claveByte := convertirABytes(clave)
+	hashing := sdbmHash(claveByte)
+	return int(hashing) % hash.tam
+}
+
+func (hash *hashCerrado[K, V]) Iterador() IterDiccionario[K, V] {
+
+	return &iterDiccionario[K, V]{}
+}
+
+func (iter *iterDiccionario[K, V]) HaySiguiente() bool {
 	return true
 }
 
-func (iter *iterDiccionario[K, V]) VerActual() (K, V){
+func (iter *iterDiccionario[K, V]) VerActual() (K, V) {
 	var clave K
 	var valor V
-	return clave,valor
+	return clave, valor
 }
 
-func (iter *iterDiccionario[K, V]) Siguiente(){
-	
+func (iter *iterDiccionario[K, V]) Siguiente() {
+
 }
-
-
