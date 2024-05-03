13920af63b7706174aaabf55f5556dc2e172a785
EnriHeller <enrique.heller12@gmail.com>
2024-05-03 18:25:28 -0300

    dalee

--- tdas/main.go
+++ tdas/main.go
@@ -6,21 +6,17 @@ import (
 )
 
 func main()  {
-	dic := TDADiccionario.CrearHash[int,int]()
+	claves := make([]string, 10)
+	cadena := "%d~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" +
+		"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
+	dic := TDADiccionario.CrearHash[string, string]()
+	valores := []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J"}
 	for i := 0; i < 10; i++ {
-		dic.Guardar(i, i)
-		fmt.Println("primer for", dic.Obtener(i))
-	}
-	for i := 0; i < 10; i++ {
-		dic.Guardar(i, 2*i)
-		fmt.Println("segundo for", dic.Obtener(i))
-	}
-	ok := true
-	for i := 0; i < 10 && ok; i++ {
-		ok = dic.Obtener(i) == 2*i
-		if !ok{
-			fmt.Println("fallo en esta clave", i)
-		}
+		claves[i] = fmt.Sprintf(cadena, i)
+
+		dic.Guardar(claves[i], valores[i])
+		fmt.Println(dic.Cantidad())
 	}
 
+	//fmt.Println(dic.Cantidad())
 }
