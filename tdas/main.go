d0fd84916371f5a04f95bde95338de9aabbda215
EnriHeller <enrique.heller12@gmail.com>
2024-05-12 23:38:00 -0300

    detalles

--- tdas/main.go
+++ tdas/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	//"fmt"
-	//"fmt"
 	"fmt"
 	TDADiccionario "tdas/diccionario"
 )
@@ -85,32 +84,51 @@ func main() {
 	dic := TDADiccionario.CrearABB[string, string](compararCadenas)
 
 	dic.Guardar(claves[0], valores[0])
-	dic.Cantidad()
-	dic.Pertenece(claves[0])
-	dic.Obtener(claves[0])
-
-	dic.Pertenece(claves[1])
 	dic.Guardar(claves[1], valores[1])
-
-	dic.Pertenece(claves[0])
-	dic.Pertenece(claves[1])
+	dic.Guardar(claves[2], valores[2])
+	fmt.Println(dic.Pertenece(claves[2]))
+	dic.Borrar(claves[2])
 
 	/*
-	require.True(t, dic.Pertenece(claves[1]))
+func TestDiccionarioBorrarABB(t *testing.T) {
+	t.Log("Guarda algunos pocos elementos en el diccionario, y se los borra, revisando que en todo momento " +
+		"el diccionario se comporte de manera adecuada")
+	clave1 := "Gato"
+	clave2 := "Perro"
+	clave3 := "Vaca"
+	valor1 := "miau"
+	valor2 := "guau"
+	valor3 := "moo"
+	claves := []string{clave1, clave2, clave3}
+	valores := []string{valor1, valor2, valor3}
+	dic := TDADiccionario.CrearABB[string, string](compararCadenas)
 
-	require.EqualValues(t, 2, dic.Cantidad())
-	require.EqualValues(t, valores[0], dic.Obtener(claves[0]))
-	require.EqualValues(t, valores[1], dic.Obtener(claves[1]))
-
-	require.False(t, dic.Pertenece(claves[2]))
+	require.False(t, dic.Pertenece(claves[0]))
+	require.False(t, dic.Pertenece(claves[0]))
+	dic.Guardar(claves[0], valores[0])
+	dic.Guardar(claves[1], valores[1])
 	dic.Guardar(claves[2], valores[2])
-	require.True(t, dic.Pertenece(claves[0]))
-	require.True(t, dic.Pertenece(claves[1]))
+
 	require.True(t, dic.Pertenece(claves[2]))
-	require.EqualValues(t, 3, dic.Cantidad())
-	require.EqualValues(t, valores[0], dic.Obtener(claves[0]))
-	require.EqualValues(t, valores[1], dic.Obtener(claves[1]))
-	require.EqualValues(t, valores[2], dic.Obtener(claves[2]))
+	require.EqualValues(t, valores[2], dic.Borrar(claves[2]))
+	require.PanicsWithValue(t, "La clave no pertenece al diccionario", func() { dic.Borrar(claves[2]) })
+	require.EqualValues(t, 2, dic.Cantidad())
+	require.False(t, dic.Pertenece(claves[2]))
+
+	require.True(t, dic.Pertenece(claves[0]))
+	require.EqualValues(t, valores[0], dic.Borrar(claves[0]))
+	require.PanicsWithValue(t, "La clave no pertenece al diccionario", func() { dic.Borrar(claves[0]) })
+	require.EqualValues(t, 1, dic.Cantidad())
+	require.False(t, dic.Pertenece(claves[0]))
+	require.PanicsWithValue(t, "La clave no pertenece al diccionario", func() { dic.Obtener(claves[0]) })
+
+	require.True(t, dic.Pertenece(claves[1]))
+	require.EqualValues(t, valores[1], dic.Borrar(claves[1]))
+	require.PanicsWithValue(t, "La clave no pertenece al diccionario", func() { dic.Borrar(claves[1]) })
+	require.EqualValues(t, 0, dic.Cantidad())
+	require.False(t, dic.Pertenece(claves[1]))
+	require.PanicsWithValue(t, "La clave no pertenece al diccionario", func() { dic.Obtener(claves[1]) })
+}
 */
 
 }
