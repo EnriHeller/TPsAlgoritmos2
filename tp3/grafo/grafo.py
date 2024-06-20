0e189714a8e66e521f47fdfc7e4b233b584a3ea6
EnriHeller <enrique.heller12@gmail.com>
2024-06-20 13:59:22 -0300

    grafo terminado

--- tp3/grafo/grafo.py
+++ tp3/grafo/grafo.py
@@ -1,7 +1,7 @@
+import random
 class Grafo:
 
     def __init__(self, dirigido = False, vertices=[]):
-
         self.dirigido= dirigido
         self.vertices = {v:{} for v in vertices}
 
@@ -17,14 +17,14 @@ class Grafo:
     
     def agregar_arista(self, v, w, peso):
         self.vertices[v][w] = peso
+        if not self.dirigido:
+            self.vertices[w][v] = peso
     
     def peso_arista(self, v, w):
         return self.vertices[v][w]
     
     def estan_unidos(self, v, w):
-        if w in self.vertices[v]:
-            return True
-        return  False
+        return w in self.vertices[v]
     
     def borrar_vertice(self, v):
         self.vertices.pop(v)
@@ -32,4 +32,11 @@ class Grafo:
             if v in vert:
                 vert.pop(v)
     
-    
+    def borrar_arista(self,v,w):
+        self.vertices[v].pop(w)
+        if not self.dirigido:
+            self.vertices[w].pop(v)
+
+    def vertice_aleatorio(self):
+        vertices = self.obtener_vertices()
+        return random.choice(vertices)
